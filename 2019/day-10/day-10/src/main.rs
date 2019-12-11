faee25859c26ad358b186dbed750ac6ddcc04201
dulyts <jobb.gabor@gmail.com>
2019-12-11 16:56:25 +0100

    day10_1 rust perf, Day 11

--- 2019/day-10/day-10/src/main.rs
+++ 2019/day-10/day-10/src/main.rs
@@ -2,67 +2,76 @@ use std::fs::{File};
 use std::io::{BufRead, BufReader};
 use std::vec::{Vec};
 use std::collections::{HashMap};
-// use std::f32;
 
-// fn parse_program(inp: &str) -> Vec<Vec<(usize,usize)>> {
-fn parse_program(inp: &str) -> Vec<(usize,usize)> {
-    let f = BufReader::new(File::open(inp).unwrap());
-
-    let arr: Vec<(usize,usize)> = f.lines()
-        .enumerate()
-        .map(|(i,l)| l.unwrap()
-            .chars()
-            .enumerate()
-            .filter(|&(_,v)| v == '#')
-            .map(|(j,_)| (i,j))
-            .collect::<Vec<(usize,usize)>>())
-        .collect::<Vec<Vec<(usize,usize)>>>()
-        .into_iter()
-        .flatten()
-        .collect();
-
-    return arr;
-}
 
 type Coord = (usize, usize);
 type AstList = Vec<Coord>;
 
-// fn process_asterodis(asteroids: &AstList, origo: &Coord) -> HashMap<String, AstList> {
-fn process_asterodis(asteroids: &AstList, origo: &Coord) -> (Coord, usize) {
-    let mut processed: HashMap<String, AstList> = HashMap::new();
+fn parse_program(inp: &str) -> AstList {
+    let f = BufReader::new(File::open(inp).unwrap());
+
+    return f.lines()
+        .enumerate()
+        .map(|(x,l)| l.unwrap()
+            .chars()
+            .enumerate()
+            .filter(|&(_,v)| v == '#')
+            .map(|(y,_)| (y, x))
+            .collect::<AstList>())
+        .collect::<Vec<_>>()
+        .into_iter()
+        .flatten()
+        .collect();
+}
+
+fn process_asterodis(asteroids: &AstList, origo: &Coord) -> HashMap<i32, AstList> {
+    let mut processed: HashMap<i32, AstList> = HashMap::new();
 
     let origo_x = origo.0 as f32;
     let origo_y = origo.1 as f32;
     for ast in asteroids {
         let ast_x = ast.0 as f32;
         let ast_y = ast.1 as f32;
+
         let degree = 360.0 - (ast_x - origo_x).atan2(ast_y - origo_y).to_degrees() - 180.0;
-        let deg_str = degree.to_string();
-        if !processed.contains_key(&deg_str) {
-            processed.insert(deg_str.clone(), Vec::new());
-        }
-        processed.get_mut(&deg_str).unwrap().push(ast.clone());
+        // let deg_str = degree.to_string();
+        // if !processed.contains_key(&deg_str) {
+        //     processed.insert(deg_str.clone(), Vec::new());
+        // }
+        // processed.get_mut(&deg_str).unwrap().push(ast.clone());
+
+        // let degree = PI - (ast_x - origo_x).atan2(ast_y - origo_y);
+        processed.entry((degree * 1_000_000.0) as i32).or_insert_with(||Vec::new()).push(ast.clone());
     }
 
-    return (origo.clone(), processed.len());
+    // return processed;
+    return processed;
 }
 
-fn part1(asteroids: &AstList) {
+fn part1(asteroids: &AstList) -> (&Coord, HashMap<i32, AstList>) {
     let result = asteroids.iter()
-        .map(|ast| process_asterodis(&asteroids, &ast))
+        .map(|ast| (ast, process_asterodis(&asteroids, &ast)))
         .into_iter()
-        .max_by_key(|a| a.1)
+        .max_by_key(|a| a.1.len())
         .unwrap();
-    println!("{:?}", result);
+    println!("{:?}", result.0);
         // .collect();
     
-    // asteroids;
+    return result;
 }
 
+// fn part2(origo: &Coord, angles: &HashMap<String, AstList>) {
+//     angles.keys().sort()
+//     // for (angle, asteroids) in angles {
+//     //     println!("{:?}", angle);
+//     // }
+// }
+
 fn main() {
-    let filename = "input.txt";
-    // let filename = "test5.txt";
-    let program = parse_program(filename);
-    part1(&program);
+    // let filename = "input.txt";
+    let filename = "test5.txt";
+    let asteroids = parse_program(filename);
+    let station_best_data = part1(&asteroids);
+    // part2(&station_best_data.0, &station_best_data.1);
     // println!("{:?}", program);
 }
