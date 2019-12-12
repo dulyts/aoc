de0b208300290d33e0553f4a42a69c276a531040
dulyts <jobb.gabor@gmail.com>
2019-12-12 12:26:33 +0100

    Modify

--- 2019/day-12/main.py
+++ 2019/day-12/main.py
@@ -44,6 +44,17 @@ def read_file():
             moons.append(Point(x,y,z))
     return moons
 
+def read_file2():
+    with open("input.txt") as f:
+        moons = []
+        for line in f:
+            line = line[1:-2]
+            params = line.split(',')
+            moons.append(int(params[0].split('=')[1]))
+            moons.append(int(params[1].split('=')[1]))
+            moons.append(int(params[2].split('=')[1]))
+    return moons
+
 def part1():
     moons = read_file()
     # moons = [Point(-1, 0, 2),Point(2, -10, -7),Point(4, -8, 8),Point(3, 5, -1)]
@@ -89,119 +100,115 @@ def part1():
     print(sum_e)
 
 
+import time
+from datetime import datetime
 
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        tsd = datetime.now()
+        result = method(*args, **kw)
+        te = time.time()
+        ted = datetime.now()
+        print('%s %s %2.2f ms' % (method.__name__, (ted - tsd), (te - ts) * 1000.0))
+        return result
+    return timed
+
+@timeit
 def part2_bruteforce():
-    moons = read_file()
+    # moons = read_file2()
+    # init_moons = [i for i in moons]
     # moons = [Point(-1, 0, 2),Point(2, -10, -7),Point(4, -8, 8),Point(3, 5, -1)]
     # moons = [Point(-8, -10,0),Point(5, 5, 10),Point(2, -7,3),Point(9, -8, -3)]
-    moons = [Point(-8, -10, 0),Point(5, 5, 10),Point(2, -7, 3),Point(9, -8, -3)]
-    for i in moons:
-        i.reset_velocity()
-        print(i)
-    print('-----')
+    # moons = [Point(-8, -10, 0),Point(5, 5, 10),Point(2, -7, 3),Point(9, -8, -3)]
     
+    init_moons = [-8,-10,0, 5,5,10, 2,-7,3, 9,-8,-3]
+    moons = [-8,-10,0, 5,5,10, 2,-7,3, 9,-8,-3]
+
+    # init_moons = [-1,0,2, 2,-10,-7, 4,-8,8, 3,5,-1]
+    # moons = [-1,0,2, 2,-10,-7, 4,-8,8, 3,5,-1]
+    velocities = [0 for i in moons]
+    NN = len(moons)
+    N = NN // 3
+    
+    print('moons', moons)
+    print('velocities', velocities)
+
     sum_e = 0
     all_on_start = False
-    time = 0
-    while not all_on_start:
-        for i in range(0, len(moons)-1):
-            for j in range(i+1, len(moons)):
-                if moons[i].x < moons[j].x:
-                    moons[i].vx += 1
-                    moons[j].vx -= 1
-                elif moons[i].x > moons[j].x:
-                    moons[i].vx -= 1
-                    moons[j].vx += 1
-                
-                if moons[i].y < moons[j].y:
-                    moons[i].vy += 1
-                    moons[j].vy -= 1
-                elif moons[i].y > moons[j].y:
-                    moons[i].vy -= 1
-                    moons[j].vy += 1
-                
-                if moons[i].z < moons[j].z:
-                    moons[i].vz += 1
-                    moons[j].vz -= 1
-                elif moons[i].z > moons[j].z:
-                    moons[i].vz -= 1
-                    moons[j].vz += 1
-        # if time % 10 == 0:
-        #     print(f'After {time} steps:')
-        #     for i in moons:
-        #         print(i)
-        for i in moons:
-            i.calc()
-            # print(i)
-        # if time % 10 == 0:
+    time = 1
 
+    N1 = (N-1)*3
+    N2 = N*3
+
+    while not all_on_start:
+        for i in range(0, N1, 3):
+            for j in range(i+3, N2, 3):
+                if moons[i] < moons[j]:
+                    velocities[i] += 1
+                    velocities[j] -= 1
+                elif moons[i] > moons[j]:
+                    velocities[i] -= 1
+                    velocities[j] += 1
+
+                ip1 = i + 1
+                ip2 = i + 2
+
+                jp1 = j + 1
+                jp2 = j + 2
+
+                if moons[ip1] < moons[jp1]:
+                    velocities[ip1] += 1
+                    velocities[jp1] -= 1
+                elif moons[ip1] > moons[jp1]:
+                    velocities[ip1] -= 1
+                    velocities[jp1] += 1
+
+                if moons[ip2] < moons[jp2]:
+                    velocities[ip2] += 1
+                    velocities[jp2] -= 1
+                elif moons[ip2] > moons[jp2]:
+                    velocities[ip2] -= 1
+                    velocities[jp2] += 1
+                
         asd = True
-        for i in moons:
-            asd = asd and i.on_start_point()
-        all_on_start = asd
-
-        time += 1
-    for i in moons:
-        print(i)
-    print(time)
-
-def part2():
-    moons = read_file()
-    # moons = [Point(-1, 0, 2),Point(2, -10, -7),Point(4, -8, 8),Point(3, 5, -1)]
-    # moons = [Point(-8, -10,0),Point(5, 5, 10),Point(2, -7,3),Point(9, -8, -3)]
-    moons = [Point(-8, -10, 0),Point(5, 5, 10),Point(2, -7, 3),Point(9, -8, -3)]
-    for i in moons:
-        i.reset_velocity()
-        print(i)
-    print('-----')
-    
-    sum_e = 0
-    all_on_start = False
-    time = 0
-    while not all_on_start:
-        for i in range(0, len(moons)-1):
-            for j in range(i+1, len(moons)):
-                if moons[i].x < moons[j].x:
-                    moons[i].vx += 1
-                    moons[j].vx -= 1
-                elif moons[i].x > moons[j].x:
-                    moons[i].vx -= 1
-                    moons[j].vx += 1
-                
-                if moons[i].y < moons[j].y:
-                    moons[i].vy += 1
-                    moons[j].vy -= 1
-                elif moons[i].y > moons[j].y:
-                    moons[i].vy -= 1
-                    moons[j].vy += 1
-                
-                if moons[i].z < moons[j].z:
-                    moons[i].vz += 1
-                    moons[j].vz -= 1
-                elif moons[i].z > moons[j].z:
-                    moons[i].vz -= 1
-                    moons[j].vz += 1
-
+        for i in range(0, NN):
+            moons[i] += velocities[i]
+            asd = asd and init_moons[i] == moons[i]
         
-        for i in moons:
-            i.calc()
-
-        asd = True
-        i = 0
-        while asd and i < len(moons):
-            asd = asd and moons[i].on_start_point2()
-        all_on_start = asd
-
-        time += 1
+        
+        # if time % 10 == 0:
+        # if time < 11:
+        #     print(f'After {time} steps:')
+        #     print('moons', moons)
+        #     print('velocities', velocities)
+            # print(i)
         if time % 10000000 == 0:
             print(time)
-            
-    for i in moons:
-        print(i)
-    print(time)
+        # print('moons', moons)
+        # print('velocities', velocities)
 
+        # asd = True
+        # for i in moons:
+        #     asd = asd and i.on_start_point()
+        # asd = True
+        # i = 0
+        # while asd and i < len(moons):
+        #     asd = asd and init_moons[i] == moons[i]
+        #     i += 1
+
+        # if asd:
+        #     print('moons', moons)
+        #     print('velocities', velocities)
+
+        all_on_start = asd
+
+        time += 1
+    # for i in moons:
+    #     print(i)
+    print('time', time)
 
 # part1()
-part2()
+part2_bruteforce()
 
 
