369e655fa99290ae91c37ad13cc665a30bd09e65
dulyts <jobb.gabor@gmail.com>
2021-12-07 11:45:32 +0100

    improve init

--- 2021/__init__/main.js
+++ 2021/__init__/main.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const path = require('path');
 
 const loadData = (filename) => {
     const data = fs
         .readFileSync(filename, "utf8")
-        .split("\n")
+        .split(/\r?\n/)
         .slice(0, -1)
         .map((d) => {
             return d;
@@ -19,8 +20,11 @@ const part2 = (data) => {
     return null;
 };
 
-const filename = "sample_0.txt";
-// const filename = "input.txt";
-const data = loadData(filename);
-console.log("part1", part1(data));
-console.log("part2", part2(data));
+fs.readdirSync(__dirname).forEach(filename => {
+    if (filename.endsWith(".txt")) {
+        console.log(filename)
+        const data = loadData(filename);
+        console.log("part1", part1(JSON.parse(JSON.stringify(data))));
+        console.log("part2", part2(JSON.parse(JSON.stringify(data))));
+    }
+})
