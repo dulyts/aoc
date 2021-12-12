2dc6a572c10672f362011f1c5e33dfabe47185dd
dulyts <jobb.gabor@gmail.com>
2021-12-12 08:29:53 +0100

    day12

---
+++ 2021/12/main.js
@@ -0,0 +1,99 @@
+const fs = require("fs");
+const path = require("path");
+const { performance } = require("perf_hooks");
+
+const loadData = (filename) => {
+    const data = fs
+        .readFileSync(filename, "utf8")
+        .split(/\r?\n/)
+        .slice(0, -1)
+        .map((d) => {
+            return d.split("-");
+        })
+        .reduce((prev, next) => {
+            if (prev[next[0]]) {
+                prev[next[0]].push(next[1]);
+            } else {
+                prev[next[0]] = [next[1]];
+            }
+            if (prev[next[1]]) {
+                prev[next[1]].push(next[0]);
+            } else {
+                prev[next[1]] = [next[0]];
+            }
+            return prev;
+        }, {});
+    // console.log(data);
+    return data;
+};
+
+const asd = (data, currentNode, path) => {
+    if (currentNode === "end") {
+        return 1;
+    }
+    let count = 0;
+    data[currentNode].forEach((d) => {
+        if (
+            d !== "start" &&
+            (d[0] === d[0].toUpperCase() || path.indexOf(d) === -1)
+        ) {
+            count += asd(data, d, [...path, d]);
+        }
+    });
+    return count;
+};
+
+const hasTwiceVisitedSmallCave = (path, p) => {
+    const smallCaveCount = path
+        .filter((d) => d[0] === d[0].toLowerCase())
+        .reduce((prev, next) => {
+            if (prev[next]) prev[next] += 1;
+            else prev[next] = 1;
+            return prev;
+        }, {});
+    const twiceVisited = Object.keys(smallCaveCount).find(
+        (d) => smallCaveCount[d] === 2
+    );
+    return !!twiceVisited;
+};
+
+const asd2 = (data, currentNode, path) => {
+    if (currentNode === "end") {
+        return 1;
+    }
+    let count = 0;
+    data[currentNode].forEach((d) => {
+        if (
+            d !== "start" &&
+            (d[0] === d[0].toUpperCase() ||
+                path.indexOf(d) === -1 ||
+                !hasTwiceVisitedSmallCave(path, d))
+        ) {
+            count += asd2(data, d, [...path, d]);
+        }
+    });
+    return count;
+};
+
+const part1 = (data) => {
+    data = JSON.parse(JSON.stringify(data));
+    const count = asd(data, "start", ["start"]);
+    return count;
+};
+const part2 = (data) => {
+    data = JSON.parse(JSON.stringify(data));
+    const count = asd2(data, "start", ["start"]);
+    return count;
+};
+
+const inputs = ["sample_0.txt", , "sample_1.txt"];
+inputs.push("input.txt");
+inputs.forEach((filename) => {
+    console.log(filename);
+    const data = loadData(filename);
+    console.log("part1", part1(data));
+    const t1 = performance.now();
+    console.log("part2", part2(data));
+    const t2 = performance.now();
+    console.log(t2 - t1, "ms");
+});
