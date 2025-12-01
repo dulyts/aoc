c6fcdd7291d921c7a02337f3e5a8aa945ada5958
dulyts <jobb.gabor@gmail.com>
2025-12-01 17:28:24 +0100

    feat: init 2025

---
+++ 2025/__init__/main.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const loadData = (filename) => {
+    const data = fs
+        .readFileSync(filename, "utf8")
+        .split(/\r?\n/)
+        .slice(0, -1)
+        .map((d) => {
+            return d;
+        });
+    console.log(data);
+    return data;
+};
+
+const part1 = (data) => {
+    data = JSON.parse(JSON.stringify(data));
+    return null;
+};
+const part2 = (data) => {
+    data = JSON.parse(JSON.stringify(data));
+    return null;
+};
+
+const inputs = [];
+inputs.push("sample_0.txt");
+// inputs.push("input.txt");
+inputs.forEach((filename) => {
+    console.log(filename);
+    const data = loadData(filename);
+    console.log("part1", part1(data));
+    console.log("part2", part2(data));
+});
